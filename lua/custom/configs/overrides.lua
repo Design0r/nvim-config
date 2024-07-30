62fca62b262dab360115246de72dda72deb11e97
alex <alex.mors@gmx.de>
2024-07-30 17:00:01 +0200

    added hmtl and css files to conform

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -69,9 +69,7 @@ M.conform = {
   default_format_opts = {
     lsp_format = "fallback",
   },
-  format_on_save = {
-    timeout_ms = 500,
-  },
+  format_on_save = {},
   formatters_by_ft = {
     lua = { "stylua" },
     -- Conform will run multiple formatters sequentially
@@ -89,6 +87,8 @@ M.conform = {
     javascript = { "prettierd", "prettier", stop_after_first = true },
     typescript = { "prettierd", "prettier", stop_after_first = true },
     typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+    html = { "prettierd", "prettier", stop_after_first = true },
+    css = { "prettierd", "prettier", stop_after_first = true },
   },
 }
 
