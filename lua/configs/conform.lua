b9b4761f77c38524d23c5af010b06f493eaf122e
alex <alex.mors@gmx.de>
2025-04-10 17:58:55 +0200

    removed conform timeout

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -32,10 +32,7 @@ local options = {
     cpp = { "clang-format" },
   },
 
-
   format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
     lsp_fallback = true,
   },
 }
