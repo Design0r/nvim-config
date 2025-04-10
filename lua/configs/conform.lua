9e9f639058e34d77fba4a564053926a67fdf9183
alex <alex.mors@gmx.de>
2025-04-10 09:24:42 +0200

    config 2025

---
+++ lua/configs/conform.lua
@@ -0,0 +1,43 @@
+local options = {
+  formatters = {
+    djlint = {
+      prepend_args = { "--indent", "2" },
+    },
+    -- ["goimports-reviser"] = {
+    --   prepend_args = { "--rm-unused" },
+    -- },
+  },
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
+  formatters_by_ft = {
+    lua = { "stylua" },
+    python = {
+      -- To fix auto-fixable lint errors.
+      "ruff_fix",
+      -- To run the Ruff formatter.
+      "ruff_format",
+      -- To organize the imports.
+      "ruff_organize_imports",
+    },
+    rust = { "rustfmt", lsp_format = "fallback" },
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+    typescript = { "prettierd", "prettier", stop_after_first = true },
+    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+    html = { "djlint", "prettierd", "prettier", stop_after_first = true },
+    css = { "prettierd", "prettier", stop_after_first = true },
+    htmldjango = { "djlint", stop_after_first = true },
+    go = { "gofumpt", "golines", "goimports-reviser" },
+    templ = { "templ" },
+    cpp = { "clang-format" },
+  },
+
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+return options
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,15 +1,20 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- if you just want default config for the servers then put them in a table
+-- EXAMPLE
 local servers = { "html", "cssls", "ts_ls", "ruff", "templ", "clangd" }
+local nvlsp = require "nvchad.configs.lspconfig"
+local on_attach = nvlsp.on_attach
+local capabilities = nvlsp.capabilities
 
+-- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
   }
 end
 
